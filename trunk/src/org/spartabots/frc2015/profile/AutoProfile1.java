e6692ba864fec6417ccda9ac73d902268ddfbea8
stevenbe99@gmail.com <stevenbe99@gmail.com@151ac6ff-986a-871b-022e-23d3b99ee23e>
2015-02-01 18:28:32 +0000

    Made it so autoProfile1 elevator goes up when picking it up and down when it gets into auto zone

--- trunk/src/org/spartabots/frc2015/profile/AutoProfile1.java
+++ trunk/src/org/spartabots/frc2015/profile/AutoProfile1.java
@@ -16,8 +16,16 @@ public class AutoProfile1 extends Profile {
 		
 		robot.elevator.clampE1In();
 		
+		robot.elevator.setE1(0.85);		//elevator up
+		robot.elevator.setE2(0.85);
+		Actions.waitAction(25);
+		
 		Actions.rotateDeg(90, 0.6); 	// Rotate robot to point to center of field
 		Actions.driveTime(6000, 0.85); 	// Drive into auto zone
+		
+		robot.elevator.setE1(-0.45);	//elevator down
+		robot.elevator.setE2(-0.45);
+		Actions.waitAction(45);
 	}
 
 	@Override
