7091f77ddedfe5bfa2efd6d617ae37982a9e5291
tommy.overland@gmail.com <tommy.overland@gmail.com@151ac6ff-986a-871b-022e-23d3b99ee23e>
2015-02-01 19:44:46 +0000

    made the autoprofile3  <3 Julia Lee

--- trunk/src/org/spartabots/frc2015/profile/AutoProfile3.java
+++ trunk/src/org/spartabots/frc2015/profile/AutoProfile3.java
@@ -1,6 +1,7 @@
 package org.spartabots.frc2015.profile;
 
 import org.spartabots.frc2015.Robot;
+import org.spartabots.frc2015.action.Actions;
 
 public class AutoProfile3 extends Profile {
 
@@ -10,7 +11,35 @@ public class AutoProfile3 extends Profile {
 	
 	@Override
 	public void init() {
+		robot.drive.resetGyro();
+		robot.drive.resetEncoders();
 		
+		robot.elevator.clampE1In();
+		robot.elevator.setE1(1);
+		robot.drive.traverse.set(-1);
+		Actions.driveTime(300, 0.85);
+		robot.drive.traverse.set(3);
+		robot.elevator.clampE1Out();
+		robot.elevator.setE1(-1);
+		
+		robot.elevator.clampE1In();
+		robot.elevator.setE1(1);
+		robot.drive.traverse.set(-1);
+		Actions.driveTime(300, 0.85);
+		robot.drive.traverse.set(1);
+		robot.elevator.clampE1Out();
+		robot.elevator.setE1(-1);
+		
+		robot.elevator.clampE1In();
+		robot.elevator.setE1(1);
+		robot.drive.traverse.set(-1);
+		Actions.driveTime(300, 0.85);
+		robot.drive.traverse.set(1);
+		robot.elevator.clampE1Out();
+		robot.elevator.setE1(-1);
+		
+		Actions.rotateDeg(-90, 0.6); 	// Rotate robot to point to center of field
+		Actions.driveTime(6000, 0.85); 	// Drive into auto zone
 	}
 
 	@Override
