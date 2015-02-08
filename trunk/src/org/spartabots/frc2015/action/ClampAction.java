8773143051d6a2dc8bdf87c8161ee822542af59b
stevenbe99@gmail.com <stevenbe99@gmail.com@151ac6ff-986a-871b-022e-23d3b99ee23e>
2015-02-08 19:07:15 +0000

    made clampState a public int to access in other classes

--- trunk/src/org/spartabots/frc2015/action/ClampAction.java
+++ trunk/src/org/spartabots/frc2015/action/ClampAction.java
@@ -3,17 +3,17 @@ package org.spartabots.frc2015.action;
 public class ClampAction extends Action {
 	public static int IN = 0;
 	public static int OUT = 1;
-	int state = 0;
+	public static int clampState = 0;
 	
 	public ClampAction(int state) {
-		this.state = state;
+		ClampAction.clampState = state;
 		if (state != 0 && state != 1)
 			throw new IllegalArgumentException();
 	}
 	
 	@Override
 	public void init() {
-		if (state == IN)
+		if (clampState == IN)
 			robot.elevator.clampIn();
 		else
 			robot.elevator.clampOut();
